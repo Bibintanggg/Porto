88cef9b87be55beb36c10d91c340cf6dca690fc4
Bibintanggg <bintangyudhaa8@gmail.com>
2024-11-10 23:32:16 +0700

    gaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Background from './assets/Background.svg';
+import Background from './assets/Background.svg'
 
 function App() {
   return (
     <div
       className='relative h-screen w-full bg-cover bg-center'
-      style={{ backgroundImage: `url(${Background})` }}
     >
       <Navbar />
       <Home />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="w-full fixed top-8 z-20 bg-opacity-50 text-lg text-white">
+        <nav className="w-full fixed top-8 z-20 bg-opacity-50 text-lg text-black">
             <ul className="flex mx-auto gap-10 text-center text-lg justify-center py-4 font-poppins font-medium">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About Me</a></li>
