2a167ae0a64272d8374365c012d9f47bddef1382
Bibintanggg <bintangyudhaa8@gmail.com>
2024-11-23 00:40:24 +0700

    fail styling

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,29 +1,34 @@
 import React from "react";
-import Medal from '../assets/3rdMedal.svg'
+import Medal from '../assets/3rdMedal.svg';
 
 function CardImage() {
     return (
-        <section>
-            <div className="justify-center h-screen items-center flex">
-                <div className="flex-col font-jakarta font-medium">
-                    <h2 className="text-center font-poppins text-4xl font-bold mb-16">Education</h2>
-
-                    <div className="flex-col">
-                    <h1 className="text-left ">2024 - Present</h1>
-                    <p>SMKN 46 - Jakarta</p>
-                    </div>
-                    
-                    <div className="w-96 h-60 bg-black mt-2"></div>    
+        <section className="h-screen flex items-center justify-center">
+            <div className="flex w-[80%] items-start justify-between">
+                {/* Kolom Kiri */}
+                <div className="flex flex-col font-jakarta font-medium w-[45%]">
+                    <h2 className="text-center font-poppins text-4xl font-bold mb-8">Education</h2>
+                    <h1 className="text-xl mb-2">SMKN 46 - Jakarta</h1>
+                    <p className="text-base mb-4">Software E</p>
+                    <div className="w-96 h-60 bg-black"></div>
                 </div>
-            <div className="flex gap-x-2">
-                <img src={Medal} alt="MedalIcon" 
-                className="w-9 ml-4"/>
-                <p className="mt-[1%]">3Rd Winner Website Competition <br /> In Trigora Cup</p>
+
+                {/* Kolom Kanan */}
+                <div className="flex flex-col font-jakarta w-[45%]">
+                    {/* Membuat "Achievement" sejajar dengan shape hitam */}
+                    <div className="flex items-start ml-8 mt-[6.5rem]">
+                        <h2 className="text-lg font-medium mr-4">Achievement</h2>
+                        <div className="flex items-center gap-x-4">
+                            <img src={Medal} alt="Medal Icon" className="w-9" />
+                            <p className="text-base">
+                                3Rd Winner Website Competition <br /> In Trigora Cup
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div> 
-            
         </section>
-    )
+    );
 }
 
 export default CardImage;
