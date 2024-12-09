8fa3728cddb212e82fb398a904ab4d829d84e4b4
Bibintanggg <bintangyudhaa8@gmail.com>
2024-12-09 09:13:37 +0700

    nyoba style

---
+++ src/components/EduColoumn.jsx
@@ -0,0 +1,13 @@
+function EduColumn() {
+    return (
+        <div className="">
+            <div>
+                <div>
+                    <p>2000 - 2912</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EduColumn
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,12 +2,20 @@ import CardImage from "../components/CardImage";
 import TextSkills from "../components/TextSkills";
 import Activity from "../components/Activity";
 import Data from "../data/ProjectData";
+import EduColumn from "../components/EduColoumn";
 
 function Discover() {
   return (
     <div>
         <CardImage/>
-        <div className="flex w-full justify-center gap-10 mx-auto bg-cover">
+        <EduColumn/>
+
+        <div className="flex mx-auto justify-center mt-[-14%]">
+        <TextSkills/>
+        </div>
+
+        <h1 className="font-poppins font-bold text-center text-4xl mt-28">Activity</h1>
+        <div className="flex w-full justify-center gap-14 mx-auto bg-cover mt-10">
           {Data.map(spill => (
           <Activity
           key={spill.id}
@@ -15,10 +23,6 @@ function Discover() {
           description={spill.description}
           image={spill.image}/>
         ))}
-        </div>
-
-        <div className="flex mx-auto justify-center">
-        <TextSkills/>
         </div>
     </div>
   );
